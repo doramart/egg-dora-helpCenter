77ea03e75f93c9a4772a1047f9f98ded3559dec2
doramart <doramart@qq.com>
2019-11-04 14:39:50 +0800

    update egg-dora-helpcenter

--- app/controller/manage/helpCenter.js
+++ app/controller/manage/helpCenter.js
@@ -8,18 +8,14 @@ const helpCenterRule = (ctx) => {
             required: true,
             min: 1,
             max: 50,
-            message: ctx.__("validate_error_field", {
-                label: ctx.__("label_tag_name")
-            })
+            message: ctx.__("validate_error_field", [ctx.__("label_tag_name")])
         },
         comments: {
             type: "string",
             required: true,
             min: 2,
             max: 200000,
-            message: ctx.__("validate_inputCorrect", {
-                label: ctx.__("label_comments")
-            })
+            message: ctx.__("validate_inputCorrect", [ctx.__("label_comments")])
         }
     }
 }
