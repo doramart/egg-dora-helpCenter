033785ee97a8f6f7266be96b191da4ae38023aff
doramart <doramart@qq.com>
2020-02-04 12:31:50 +0800

    update egg-dora-helpcenter

--- app/middleware/helpCenterRouter.js
+++ app/middleware/helpCenterRouter.js
@@ -6,7 +6,7 @@ module.exports = (options, app) => {
     return async function helpCenterRouter(ctx, next) {
 
         let pluginConfig = app.config.doraHelpCenter;
-        await ctx.initPluginRouter(pluginConfig, helpCenterAdminController);
+        await app.initPluginRouter(ctx, pluginConfig, helpCenterAdminController);
         await next();
 
     }
